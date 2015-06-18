2931573b971754895d3f48b89ad6f8b9ba49b8de
boaromayo <nly_poblet@yahoo.com>
2015-06-18 14:03:19 -0700

    Update FruitEditor.java

--- src/FruitEditor.java
+++ src/FruitEditor.java
@@ -3,30 +3,11 @@ import javax.swing.*;
 
 public class FruitEditor {
   
-  // VARIABLES.
-  public static final int WIDTH = 800;
-  public static final int HEIGHT = 600;
-  
   // MAIN FRAME.
-  private JFrame fruitFrame;
-  
-  // PANELS.
-  private FruitPanel fruitPanel;
-  /*private TilePanel tilePanel;
-  private FilePanel filePanel;
-  private MapPanel mapPanel;*/
+  private FruitFrame fruitFrame;
   
   public FruitEditor() {
-    fruitFrame = new JFrame("FruitEditor");
-    fruitPanel = new FruitPanel();
-    
-    fruitFrame.pack();
-    
-    fruitFrame.add(fruitPanel);
-    fruitFrame.setJMenuBar(fruitPanel.getMenuBar());
-    
-    fruitFrame.setLocationRelativeTo(null);
-    fruitFrame.setVisible(true);
-    fruitFrame.setResizable(false);
+    fruitFrame = new FruitFrame("FruitEditor");
   }
+
 }
