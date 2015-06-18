d0dea6a2e4312c5f2ce19b62d190c329d4f4ddb4
boaromayo <nly_poblet@yahoo.com>
2015-06-18 14:13:57 -0700

    Update FruitEditor.java

--- src/FruitEditor.java
+++ src/FruitEditor.java
@@ -1,13 +1,24 @@
-import java.awt.*;
 import javax.swing.*;
+import java.util.*;
 
-public class FruitEditor {
+public class FruitEditor implements Runnable {
   
   // MAIN FRAME.
   private FruitFrame fruitFrame;
   
   public FruitEditor() {
-    fruitFrame = new FruitFrame("FruitEditor");
+    fruitFrame = new FruitFrame();
+  }
+  
+  public void run() {
+    try {
+      while (true) {
+        
+      }
+    } catch (Exception e) {
+      System.err.println("ERROR: " + e.getMessage());
+      e.printStackTrace();
+    }
   }
 
 }
