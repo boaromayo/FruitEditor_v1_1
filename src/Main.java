833a7d884b79bf7e059718e0c14774234efdfc97
boaromayo <nly_poblet@yahoo.com>
2015-06-18 12:10:34 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,7 @@ import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
-    EditorPanel ep = new EditorPanel();
-    JFrame frame = new JFrame("FruitEditor");
-    
-    frame.add(ep);
-    frame.pack();
-    
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setVisible(true);
+    FruitEditor o = new FruitEditor();
+    o.start();
   }
 }
