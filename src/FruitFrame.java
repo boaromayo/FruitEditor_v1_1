3ff8672066a9b180334048f237689f50aefabc79
boaromayo <nly_poblet@yahoo.com>
2015-06-18 18:31:43 -0700

    Update FruitFrame.java

--- src/FruitFrame.java
+++ src/FruitFrame.java
@@ -6,79 +6,79 @@ import javax.swing.*;
 import javax.swing.event.*;
 
 public class FruitFrame extends JFrame {
-  // MAIN PANELS: FRUITPANEL, MAPPANEL, TILEPANEL, MAPLISTPANEL.
-  private FruitPanel fruitPanel;
-  private MapPanel mapPanel;
-  private TilePanel tilePanel;
-  private MapListPanel mapListPanel;
+    // MAIN PANELS: FRUITPANEL, MAPPANEL, TILEPANEL, MAPLISTPANEL.
+    private FruitPanel fruitPanel;
+    private MapPanel mapPanel;
+    private TilePanel tilePanel;
+    private MapListPanel mapListPanel;
   
-  // PANES -> SCROLL PANES.
-  private JScrollPane mapScroller;
-  private JScrollPane tileScroller;
-  private JScrollPane mapListScroller;
-  // PANES -> SPLIT PANES.
-  private JSplitPane splitter;
+    // PANES -> SCROLL PANES.
+    private JScrollPane mapScroller;
+    private JScrollPane tileScroller;
+    private JScrollPane mapListScroller;
+    // PANES -> SPLIT PANES.
+    private JSplitPane splitter;
   
-  // OUTER PANELS.
-  private JPanel leftPanel;
-  private JPanel rightPanel;
+    // OUTER PANELS.
+    private JPanel leftPanel;
+    private JPanel rightPanel;
   
-  public FruitFrame() {
-    // Initialize panels.
-    initPanel();
+    public FruitFrame() {
+        // Initialize panels.
+        initPanel();
     
-    setTitle("FruitEditor");
-    pack();
+        setTitle("FruitEditor");
+        pack();
     
-    setMenuBar(fruitPanel.getMenuBar());
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setLocationRelativeTo(null);
-    setVisible(true);
-  }
+        setMenuBar(fruitPanel.getMenuBar());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
   
-  private void initPanel() {
-    fruitPanel = new FruitPanel();
-    mapPanel = new MapPanel();
-    tilePanel = new TilePanel();
-    mapListPanel = new MapListPanel();
+    private void initPanel() {
+        fruitPanel = new FruitPanel();
+        mapPanel = new MapPanel();
+        tilePanel = new TilePanel();
+        mapListPanel = new MapListPanel();
     
-    mapScroller = new JScrollPane(mapPanel);
-    tileScroller = new JScrollPane(tilePanel);
-    mapListScroller = new JScrollPane(mapListPanel);
+        mapScroller = new JScrollPane(mapPanel);
+        tileScroller = new JScrollPane(tilePanel);
+        mapListScroller = new JScrollPane(mapListPanel);
     
-    fruitPanel.setLayout(new BorderLayout());
+        fruitPanel.setLayout(new BorderLayout());
     
-    leftPanel = new JPanel(new FlowLayout());
-    rightPanel = new JPanel(new BorderLayout());
+        leftPanel = new JPanel(new FlowLayout());
+        rightPanel = new JPanel(new BorderLayout());
     
-    leftPanel.add(tileScroller);
-    leftPanel.add(mapListScroller);
-    rightPanel.add(mapScroller);
+        leftPanel.add(tileScroller);
+        leftPanel.add(mapListScroller);
+        rightPanel.add(mapScroller);
     
-    // Make a JSplitPane to divide the tileset and the map panels.
-    // Tileset and map list panels go left, map panel goes right.
-    splitter = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftPanel, rightPanel);
-    splitter.setDividerLocation(240);
+        // Make a JSplitPane to divide the tileset and the map panels.
+        // Tileset and map list panels go left, map panel goes right.
+        splitter = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, leftPanel, rightPanel);
+        splitter.setDividerLocation(240);
     
-    add(fruitPanel, BorderLayout.CENTER);
-    add(splitter, BorderLayout.CENTER);
-  }
+        add(fruitPanel, BorderLayout.CENTER);
+        add(splitter, BorderLayout.CENTER);
+    }
   
-  //==============================
-  // GET PANELS.
-  //==============================
-  public MapPanel getMapPanel() {
-    if (mapPanel == null) { return null; }
-    return mapPanel;
-  }
+    //==============================
+    // GET PANELS.
+    //==============================
+    public MapPanel getMapPanel() {
+        if (mapPanel == null) { return null; }
+        return mapPanel;
+    }
   
-  public TilePanel getTilePanel() {
-    if (tilePanel == null) { return null; }
-    return tilePanel;
-  }
+    public TilePanel getTilePanel() {
+        if (tilePanel == null) { return null; }
+        return tilePanel;
+    }
   
-  public MapListPanel getMapListPanel() {
-    if (mapListPanel == null) { return null; }
-    return mapListPanel;
-  }
+    public MapListPanel getMapListPanel() {
+        if (mapListPanel == null) { return null; }
+        return mapListPanel;
+    }
 }
