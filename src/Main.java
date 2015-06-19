9a8976e225b0117b524a5128a72185592d8eb2ac
boaromayo <nly_poblet@yahoo.com>
2015-06-18 18:34:56 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
+package FruitEditor;
+
 import java.util.*;
 
 public class Main {
-  public static void main(String[] args) {
-    new FruitEditor();
-  }
+    public static void main(String[] args) {
+        new FruitEditor();
+    }
 }
