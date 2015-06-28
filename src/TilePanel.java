5d472d4e6bdcad4d2143378bafafd7c07ec26430
boaromayo <nly_poblet@yahoo.com>
2015-06-28 10:21:49 -0700

    Create TilePanel.java

---
+++ src/TilePanel.java
@@ -0,0 +1,14 @@
+package FruitEditor;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class TilePanel extends JPanel {
+	
+	public TilePanel() {
+	
+	}
+	
+}
