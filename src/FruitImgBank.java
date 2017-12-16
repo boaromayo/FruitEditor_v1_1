00aa06a6e3284beba60d061f9603c424e678dbee
Nico Poblete <nlypoblet@gmail.com>
2017-12-16 10:58:49 -0800

    Optimized map drawing (#43)
    * Fixed grid settings dialog bug
    
    * Fixed tileset dialogs
    
    * Added 'delete' functionality; cleaned up code
    
    * Added test build badge
    
    * Minor fixes to badge source
    
    * Added wandring-tile.gif
    
    * Added flood fill feature; optimized map drawing
    
    * Fixed map drawing bug
    
    * Added modulus comment
    
    * Optimized map drawing

--- src/FruitImgBank.java
+++ src/FruitImgBank.java
@@ -1,5 +1,6 @@
 package FruitEditor;
 
+import java.awt.*;
 import java.awt.image.*;
 
 import java.io.*;
@@ -61,8 +62,9 @@ public class FruitImgBank {
 			fis = new FileInputStream(file);
 			System.out.println("Loading image...");
 			img = ImageIO.read(fis);
+			BufferedImage newimg = convertToARGB(img, 0, 0);
 			fis.close();
-			return img;
+			return newimg;
 		} catch (Exception e) {
 			System.err.println("ERROR: Unable to load buffered image " + path);
 			e.printStackTrace();
@@ -74,7 +76,7 @@ public class FruitImgBank {
 	//====================================
 	// loadBufferedImage(path,x,y,width,height) - Load a BufferedImage, find (x,y), and note the width and height.
 	//====================================
-	public BufferedImage loadBufferedImage(String path, int x, int y, int width, int height) {
+	public BufferedImage loadBufferedImage(String path, int x, int y) {
 		File file;
 		FileInputStream fis;
 		BufferedImage img;
@@ -83,14 +85,29 @@ public class FruitImgBank {
 			file = new File(path);
 			fis = new FileInputStream(file);
 			img = ImageIO.read(fis);
-			img = img.getSubimage(x, y, width, height);
+			BufferedImage newimg = convertToARGB(img, x, y);
 			fis.close();
-			return img;
+			return newimg;
 		} catch (Exception e) {
 			System.err.println("ERROR: Unable to load buffered image " + path);
 			e.printStackTrace();
 		}
 		
 		return null;
+	}
+	
+	//====================================
+	// convertToARGB(img,x,y) - Convert BufferedImage to ARGB type for faster drawing, and draw image from (x,y).
+	// Special thanks to Marco13 from this tutorial:
+	// https://stackoverflow.com/questions/22230866/java-game-images-loading-very-slowly/22231370#
+	//====================================
+	public BufferedImage convertToARGB(BufferedImage img, int x, int y) {
+		BufferedImage newimg = new BufferedImage(img.getWidth(), 
+				img.getHeight(), 
+				BufferedImage.TYPE_INT_ARGB);
+		Graphics2D g = newimg.createGraphics();
+		g.drawImage(img, x, y, null);
+		g.dispose();
+		return newimg;
 	}
 }
